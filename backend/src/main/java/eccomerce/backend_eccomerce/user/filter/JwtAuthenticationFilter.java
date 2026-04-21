0f23ea1b491f2df2d863063b399239880eb613ba
rotxiv <rotxiv0@gmail.com>
2026-04-21 14:48:13 -0400

    Prueba de commit: Característica: añadir la anotación NonNull a HttpServletRequest, HttpServletResponse y FilterChain en JwtAuthenticationFilter

--- backend/src/main/java/eccomerce/backend_eccomerce/user/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/eccomerce/backend_eccomerce/user/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package eccomerce.backend_eccomerce.user.filter;
 
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,7 +29,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) 
     throws ServletException, IOException {
         String jwt = getJWTFromRequest(request);
 
